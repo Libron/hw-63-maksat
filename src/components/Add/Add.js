abd9be818458dbc8c38983cef5bf3497f55dfc84
Maksat Duishenaliev <maksatovy@gmail.com>
2019-01-30 16:10:55 +0600

    Нужно настроить отправку на сервер

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
-import { EditorState} from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
+import React, {Component} from 'react';
+import {EditorState} from 'draft-js';
+import {Editor} from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import axios from "axios";
 
 class Add extends Component {
     state = {
         editorState: EditorState.createEmpty(),
+        title: '',
+        description: [],
     };
 
     onEditorStateChange = (editorState) => {
@@ -13,22 +16,51 @@ class Add extends Component {
             editorState,
         });
     };
+
+    titleSave = (event) => {
+        this.setState({title: event.currentTarget.value})
+    };
+
+    titleDescription = (event) => {
+
+        this.setState({description: event.blocks.map(item => {
+                return item.text
+            })});
+            //Текст у Wysiwyg находится в массиве blocks
+        // Каждая новая строка это новый массив элемента
+    };
+
+    sendToApi = (event) => {
+        event.preventDefault();
+        const article = {
+            title: this.state.title,
+            description: this.state.description,
+        };
+
+        axios.post('article.json', article).then(()=>{
+            this.props.history.push('/');
+        });
+      console.log(this.state);
+    };
+
     render() {
         const {editorState} = this.state;
         return (
             <div className='add__container'>
                 <div className='title__div'>
-                    <p>Add new post</p>
-                    <label>Title</label>
-                    <input className='input__title'/>
-                    <label> Description</label>
-                    <Editor
-                        editorState={editorState}
-                        toolbarClassName="toolbarClassName"
-                        wrapperClassName="wrapperClassName"
-                        editorClassName="editorClassName"
-                        onEditorStateChange={this.onEditorStateChange}
-                    />
+                    <form >
+                        <p>Add new post</p>
+                        <label>Title</label>
+                        <input className='input__title' type="text" name="title" value={this.state.name} onChange={event=>this.titleSave(event)}/>
+                        <label> Description</label>
+                        <Editor
+                            editorState={editorState}
+                            toolbarClassName="toolbarClassName"
+                            wrapperClassName="wrapperClassName"
+                            editorClassName="editorClassName"
+                            onEditorStateChange={this.onEditorStateChange} onChange={event=>this.titleDescription(event)} />
+                        <button type='button' onClick={event=>this.sendToApi(event)}> Submit</button>
+                    </form>
                 </div>
 
             </div>
